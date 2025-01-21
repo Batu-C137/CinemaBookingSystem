117c99a0367bea51b1368fed8afa6624d3a686d9
Batu-C137 <only_games92@gmx.de>
2025-01-21 21:02:02 +0100

    Update console menu and movie list file.

--- CinemaSystem.cpp
+++ CinemaSystem.cpp
@@ -14,7 +14,7 @@ void CinemaSystem::AddMovie(const Movie& movie)
 {
 	string movieInfo = "\t\t" + movie.getTitel() + "\t\t\t" + movie.getGenre() + "\t\t\t" + to_string(movie.getDauer());
 	//string header = "Nr:\t\tTitel:\t\t\tGenre:\t\t\tLength(min.):";
-	string header[4] = { "Nr:", "Titel:", "Genre:", "Length(min.):" }; // new header
+	string header[4] = { "Nr:", "Titel:", "Genre:", "Length(min.):" }; // implement new header
 	fileManager.WriteFile(movieInfo, fileManager.movieListName, header);
 }
 
